5a929c15a578b819a6e2b92dca8170ea4dac23e2
Dipu Krishnan <dipudan@gmail.com>
2024-09-25 14:41:22 +0200

    Update google_sheet.py

--- google_sheet.py
+++ google_sheet.py
@@ -14,17 +14,16 @@ try:
     spreadsheet_id = os.environ.get('GOOGLE_SHEET_ID')
     sheet_content = os.environ.get("GOOGLE_SHEET_TOKEN")
     
-    if spreadsheet_id == '1IjdhROMebuEJvryiPsdBgsgo_CniuD-xJGaXRNUh0cA':
-        range_name = 'Sheet1!A:A'
-        
-        credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
-        service = discovery.build('sheets', 'v4', credentials=credentials)
-        
-        values = [['Dev_Fetch', 'PROD', '0.304929', '2024-08-27']]
-        data = {'values': values}
-        
-        service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, body=data, range=range_name,
-                                               valueInputOption='USER_ENTERED').execute()
+    range_name = 'Sheet1!A:A'
+    
+    credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
+    service = discovery.build('sheets', 'v4', credentials=credentials)
+    
+    values = [['Dev_Fetch', 'PROD', '0.304929', '2024-08-27']]
+    data = {'values': values}
+    
+    service.spreadsheets().values().append(spreadsheetId=spreadsheet_id, body=data, range=range_name,
+                                           valueInputOption='USER_ENTERED').execute()
 
 except OSError as e:
     print(e)
